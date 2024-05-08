56a094e64217ce5cbfd989c4fb8b75dfce4ac710
jintao_xiao <jintao.xiao@sap.com>
2024-05-08 21:38:03 +0800

    do some fix to adapt rio

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,25 @@
 module Main (main) where
 
 import System.Environment
-import Lib (process, Config(..))
-import RIO
+import Lib (run)
+import RIO.Directory (getHomeDirectory)
+import RIO.FilePath ((</>))
+
+-- only for MacOS
+netEaseMusicCachePath :: String
+netEaseMusicCachePath = "Library/Containers/com.netease.163music/Data/Caches/online_play_cache"
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [src, dest] -> do
-      putStrLn $ "src: " ++ src ++ ", dest: " ++ dest
-      let config = Config src dest
-      runRIO config $ process
+    ["--version"] -> putStrLn "0.0.1"
+    [dest] -> do
+      home <- getHomeDirectory
+      let src = home </> netEaseMusicCachePath
+      run src dest
+    [src, dest] -> run src dest
     _ -> putStrLn "print usage"
+
+
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,34 +1,69 @@
 {-# LANGUAGE OverloadedStrings, NoImplicitPrelude #-}
 
-module Lib (process, Config(..)) where
+module Lib (run) where
 
 import Text.Regex.Posix ((=~))
--- import Text.Regex.Posix.ByteString.Lazy
-import Control.Monad (forM_)
 import NetEase (MusicCacheInfo, musicName)
 import Data.Aeson (decode)
 import Data.Bits (xor)
-import Data.Word (Word8)
 import RIO
-import RIO.FilePath
-import RIO.Directory (listDirectory)
+    ( (++),
+      stdout,
+      lens,
+      filter,
+      traceIO,
+      ($),
+      Monad(return),
+      Bool(..),
+      String,
+      Maybe(..),
+      IO,
+      Word8,
+      (>>),
+      (<$>),
+      (.),
+      error,
+      FilePath,
+      MonadIO(..),
+      forM_,
+      logInfo,
+      runRIO,
+      MonadReader(ask),
+      RIO,
+      logOptionsHandle,
+      SimpleApp, HasLogFunc, LogFunc, setLogUseTime, withLogFunc, HasLogFunc(..), displayShow)
+import RIO.FilePath ( (</>), takeBaseName, takeDirectory )
+import RIO.Directory (listDirectory, getHomeDirectory)
 import qualified RIO.ByteString.Lazy as L
+import qualified RIO.Text as LT
 
 data Config = Config
-    { musicCachePath :: FilePath
+    { logFunc :: !LogFunc
+    , musicCachePath :: FilePath
     , targetDir :: FilePath }
+
+instance HasLogFunc Config where
+  logFuncL = lens logFunc (\x y -> x { logFunc = y })
+
+run :: String -> String -> IO ()
+run src dest = do
+  rawlogOptions <- logOptionsHandle stdout True
+  let logOptions = setLogUseTime True rawlogOptions
+  withLogFunc logOptions $ \lf -> do
+    let conf = Config lf src dest
+    runRIO conf process
 
 process :: RIO Config ()
 process  = do
-    Config src dest <- ask
+    Config _ src dest <- ask
     ucFiles <- getUcFiles src
     forM_ ucFiles $ \file -> convert file dest
-    --logInfo "finish process"
+    logInfo "finish process"
 
 -- (xxx.uc!, xxx.info) -> (xxx.mp3)
 convert :: FilePath -> FilePath -> RIO Config ()
 convert file dest = do
-    contents <- {- traceIO ("convert: " ++ file) >> -} liftIO $ L.readFile file
+    contents <- {- logInfo (displayShow $ "convert: " ++ file) >> -} liftIO $ L.readFile file
     let bytes = L.pack $ unmask <$> L.unpack contents
     info <- loadInfo . infoName $ file
     let songName = case info of
@@ -50,7 +85,8 @@ isUc path = path =~ (".uc!" :: String)
 infoName :: FilePath -> FilePath
 infoName ucFile = takeDirectory ucFile </> takeBaseName ucFile ++ ".info"
 
-loadInfo :: FilePath -> RIO Config (Maybe MusicCacheInfo)
+loadInfo ::  FilePath -> RIO Config (Maybe MusicCacheInfo)
 loadInfo file = do
-  contents <- {- traceIO ("findInfo: " ++ file) >> -} liftIO $ L.readFile file
+  logInfo (displayShow $ "findInfo: " ++ file)
+  contents <- liftIO $ L.readFile file
   return . decode $ contents
